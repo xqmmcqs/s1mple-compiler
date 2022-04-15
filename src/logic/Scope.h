8287be85a36a5b5a699769fd0418051bd51e2648
Dasixxbb <2570153857@qq.com>
2022-04-15 18:51:15 +0800

    procedureAndFunctionDeclaration

--- src/logic/Scope.h
+++ src/logic/Scope.h
@@ -11,10 +11,6 @@ namespace PascalS
     public:
         std::unordered_map<std::string, llvm::Value *> variables;
         
-        llvm::Function *currentFunction = nullptr;
-
-        Scope(llvm::Function *_currentFunction = nullptr) : currentFunction(_currentFunction) {}
-
         llvm::Value *setVariable(const std::string &name, llvm::Value *);
 
         llvm::Value *getVariable(const std::string &name);
