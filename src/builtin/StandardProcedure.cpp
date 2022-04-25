75804dc219b3dcf76e2155d2a53aac9cfdeaaa4c
xqmmcqs <xqmmcqs@outlook.com>
2022-04-25 19:10:45 +0800

    update stdprocedure

--- src/builtin/StandardProcedure.cpp
+++ src/builtin/StandardProcedure.cpp
@@ -8,14 +8,16 @@
 
 using namespace PascalS;
 
-std::unordered_map<std::string, llvm::Function *(*)(std::unique_ptr<llvm::Module>)> StandardProcedure::prototypeMap;
+std::unordered_map<std::string, llvm::Function *(*)(llvm::Module *)> StandardProcedure::prototypeMap;
 
 std::unordered_map<std::string, void (*)(llvm::IRBuilder<> *, std::vector<llvm::Value *> &)> StandardProcedure::argsConstructorMap;
 
 void StandardProcedure::init()
 {
     prototypeMap["writeln"] = writelnPrototype;
-    argsConstructorMap["writeln"] = writelnArgsConstructor;
+    prototypeMap["readln"] = writelnPrototype;
+    argsConstructorMap["writeln"] = readlnArgsConstructor;
+    argsConstructorMap["readln"] = readlnArgsConstructor;
 }
 
 bool StandardProcedure::hasProcedure(std::string name)
@@ -23,8 +25,35 @@ bool StandardProcedure::hasProcedure(std::string name)
     return prototypeMap.find(name) != prototypeMap.end();
 }
 
+llvm::Function *StandardProcedure::readlnPrototype(llvm::Module * module)
+{
+    auto scanf_type = llvm::FunctionType::get(llvm::Type::getVoidTy(module->getContext()), {llvm::Type::getInt8PtrTy(module->getContext())}, true);
+    auto func = module->getOrInsertFunction("scanf", scanf_type, llvm::AttributeList().addAttribute(module->getContext(), 1U, llvm::Attribute::NoAlias));
 
-llvm::Function *StandardProcedure::writelnPrototype(std::unique_ptr<llvm::Module> module)
+    return llvm::cast<llvm::Function>(func.getCallee());
+}
+
+void StandardProcedure::readlnArgsConstructor(llvm::IRBuilder<> *builder, std::vector<llvm::Value *> &args)
+{
+    std::vector<std::string> formats;
+
+    for (const auto &arg : args)
+    {
+        auto type = arg->getType();
+        if (type->isIntegerTy())
+            formats.push_back("%d");
+        else
+            throw NotImplementedException();
+    }
+
+    std::ostringstream format;
+    std::copy(formats.begin(), formats.end(), std::ostream_iterator<std::string>(format));
+    auto formatString = format.str();
+
+    args.insert(args.begin(), builder->CreateGlobalStringPtr(formatString));
+}
+
+llvm::Function *StandardProcedure::writelnPrototype(llvm::Module * module)
 {
     auto printf_type = llvm::FunctionType::get(llvm::Type::getVoidTy(module->getContext()), {llvm::Type::getInt8PtrTy(module->getContext())}, true);
     auto func = module->getOrInsertFunction("printf", printf_type, llvm::AttributeList().addAttribute(module->getContext(), 1U, llvm::Attribute::NoAlias));
